f908c8f07c0f09caf871e3d4612e15a6aad7a1c0
jangji96 <xxwlgns@naver.com>
2021-08-09 15:51:04 +0900

    2020.08.09

--- BOJ/2557.py
+++ BOJ/2557.py
@@ -1,9 +1,14 @@
-# Hello World
+'''
+Hello World
 
-# 문제
-# Hello World!를 출력하시오.
+문제
+Hello World!를 출력하시오.
 
-# 출력
-# Hello World!를 출력하시오.
+출력
+Hello World!를 출력하시오.
+
+예제 출력
+Hello World!
+'''
 
 print('Hello World!')
